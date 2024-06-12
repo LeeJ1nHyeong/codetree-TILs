2dd01f1e865ac4efb428fc7438e0446cf9151b83
이진형 <122415954+LeeJ1nHyeong@users.noreply.github.com>
2024-06-12 15:33:15 +0900

    Title: 나무 타이쿤, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

--- 240612/나무 타이쿤/tree-tycoon.py
+++ 240612/나무 타이쿤/tree-tycoon.py
@@ -3,9 +3,15 @@ def move_nutrient():
     next_nutrient = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
+            # 특수 영양제를 d방향으로 p칸 이동
             if nutrient[i][j]:
+                # 범위를 벗어나면 반대편과 이어져있다고 가정
                 ni, nj = (i + di[d] * p) % n, (j + dj[d] * p) % n
+
+                # 이동한 위치에 특수 영양제 표시
                 next_nutrient[ni][nj] = 1
+
+                # 특수 영양제를 이동한 자리의 리브로수 높이 1 증가
                 tree[ni][nj] += 1
 
     return next_nutrient
@@ -16,17 +22,21 @@ def grow():
     next_tree = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
+            # 특수 영양제가 있는 위치에서 인접한 대각선 4방향 탐색
             if nutrient[i][j]:
                 grown_tree = tree[i][j]
                 for di, dj in (-1, -1), (-1, 1), (1, -1), (1, 1):
                     ni, nj = i + di, j + dj
 
+                    # 범위를 벗어난 지역은 제외
                     if ni < 0 or ni == n or nj < 0 or nj == n:
                         continue
 
+                    # 높이가 1 이상인 리브로수 탐색
                     if tree[ni][nj]:
                         grown_tree += 1
 
+                # 높이가 1 이상인 리브로수 개수만큼 성장
                 next_tree[i][j] += grown_tree
 
             else:
@@ -39,13 +49,16 @@ def grow():
 def cut():
     for i in range(n):
         for j in range(n):
+            # 특수 영양제가 없는 위치 탐색
             if not nutrient[i][j]:
+                # 높이가 2 이상인 리브로수에 대해 2 감소 후 해당 위치에 특수 영양제 부여
                 if tree[i][j] < 2:
                     continue
 
                 tree[i][j] -= 2
                 nutrient[i][j] = 1
-                
+            
+            # 기존에 특수 영양제가 있었다면 특수 영양제 회수
             else:
                 nutrient[i][j] = 0
 
