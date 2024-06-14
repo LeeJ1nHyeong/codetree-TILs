f24e2c88efd00f4befcce6a27b445ac0e6528e4f
이진형 <122415954+LeeJ1nHyeong@users.noreply.github.com>
2024-06-14 20:48:04 +0900

    Title: 마법의 숲 탐색, Time: 344ms, Memory: 29MB, Status: Passed - CodeTree

--- 240614/마법의 숲 탐색/magical-forest-exploration.py
+++ 240614/마법의 숲 탐색/magical-forest-exploration.py
@@ -9,7 +9,7 @@ forest = [[0] * c for _ in range(r + 3)]
 g_di = [-1, 0, 1, 0]
 g_dj = [0, 1, 0, -1]
 
-ans = 0
+ans = 0  # 최대로 위치한 행의 총 합
 
 # 숲 초기화
 def clear_forest():
@@ -17,19 +17,29 @@ def clear_forest():
         for j in range(c):
             forest[i][j] = 0
 
+# 조건 확인
+def check(ni, nj):
+    # 범위 밖을 벗어나거나 골렘이 있다면 False return
+    if ni < 0 or ni >= r + 3 or nj < 0 or nj >= c:
+        return False
+
+    if forest[ni][nj]:
+        return False
+
+    return True
+
 
 # 아래로 이동
 def down(i, j):
     # 아래쪽 3곳 비었는지 확인
     for di, dj in (1, -1), (2, 0), (1, 1):
         ni, nj = i + di, j + dj
-        if ni < 0 or ni >= r + 3 or nj < 0 or nj >= c:
+
+        # 이동 조건을 만족하지 못했다면 False return
+        if not check(ni, nj):
             return False
 
-        if forest[ni][nj]:
-            return False
-
-    return True
+    return True  # 이동 조건을 만족했다면 True return
 
 
 # 왼쪽으로 회전
@@ -38,19 +48,17 @@ def rotate_left(i, j):
     # 왼쪽 3곳 비었는지 확인
     for di, dj in (1, -1), (0, -2), (-1, -1):
         ni, nj = i + di, j + dj
-        if ni < 0 or ni >= r + 3 or nj < 0 or nj >= c:
-            return False
 
-        if forest[ni][nj]:
+        # 이동 조건을 만족하지 못했다면 False return
+        if not check(ni, nj):
             return False
 
     # 이 후 아래쪽 2곳 비었는지 확인
     for di, dj in (1, -2), (2, -1):
         ni, nj = i + di, j + dj
-        if ni < 0 or ni >= r + 3 or nj < 0 or nj >= c:
-            return False
 
-        if forest[ni][nj]:
+        # 이동 조건을 만족하지 못했다면 False return
+        if not check(ni, nj):
             return False
 
     return True
@@ -62,42 +70,43 @@ def rotate_right(i, j):
     # 오른쪽 3곳 비었는지 확인
     for di, dj in (1, 1), (0, 2), (-1, 1):
         ni, nj = i + di, j + dj
-        if ni < 0 or ni >= r + 3 or nj < 0 or nj >= c:
-            return False
 
-        if forest[ni][nj]:
+        # 이동 조건을 만족하지 못했다면 False return
+        if not check(ni, nj):
             return False
 
     # 이 후 아래쪽 2곳 비었는지 확인
     for di, dj in (1, 2), (2, 1):
         ni, nj = i + di, j + dj
-        if ni < 0 or ni >= r + 3 or nj < 0 or nj >= c:
-            return False
 
-        if forest[ni][nj]:
+        # 이동 조건을 만족하지 못했다면 False return
+        if not check(ni, nj):
             return False
 
     return True
 
 
 for num in range(1, k + 1):
-    j, d = map(int, input().split())
+    j, d = map(int, input().split())  # 시작 열, 출구 방향
     j -= 1
 
-    # 이동할 수 없을 때까지 아래, 왼쪽회전, 오른쪽 회전 진행
+    # 이동할 수 없을 때까지 아래, 왼쪽 회전, 오른쪽 회전 진행
     next_d = d
     i = 1
     while True:
+        # 아래로 이동
         if down(i, j):
             i += 1
             continue
 
+        # 왼쪽 회전 시 출구 반시계 방향으로 회전
         if rotate_left(i, j):
             i += 1
             j -= 1
             next_d = (next_d - 1) % 4
             continue
 
+        # 오른쪽 회전 시 출구 시계 방향으로 회전
         if rotate_right(i, j):
             i += 1
             j += 1
@@ -110,8 +119,6 @@ for num in range(1, k + 1):
     if i <= 3:
         clear_forest()
         continue
-
-    # print(i, j, next_d)
 
     # 정령 시작 위치
     spirit_i, spirit_j = i, j
@@ -126,21 +133,19 @@ for num in range(1, k + 1):
         else:
             forest[ni][nj] = num
 
-    # print("forest----------")
-    # for f in forest:
-    #     print(*f)
-
     # 정령 이동
-    ## 절대값이 같은 곳은 자유 이동
-    ## 절대값이 다를 때 현재 위치가 출구(음수값)이라면 이동 가능
+    ## 절댓값이 같은 곳은 자유 이동
+    ## 절댓값이 다를 때 현재 위치가 출구(음수값)이라면 이동 가능
     ## 위 세줄 추가한 거 생각해야함
 
+    # bfs 초기 세팅
     visited = [[0] * c for _ in range(r + 3)]
     visited[spirit_i][spirit_j] = 1
     queue = deque([(spirit_i, spirit_j)])
 
     max_i = 0  # 최대한 남쪽으로 이동했을 때의 행
 
+    # bfs 진행
     while queue:
         ci, cj = queue.popleft()
 
@@ -164,13 +169,10 @@ for num in range(1, k + 1):
             visited[ni][nj] = 1
             queue.append((ni, nj))
 
+            # 최대로 남쪽으로 이동했는지 확인
             max_i = max(max_i, ni - 2)
 
-    # print("max_i----------")
-    # print(max_i)
+    ans += max_i  # 최대 행 더해주기
 
-    ans += max_i
-
-    # print()
-
+# 최대로 위치한 행의 총 합
 print(ans)
