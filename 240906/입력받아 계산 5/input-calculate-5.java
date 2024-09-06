25a4b8968269657e43ce21009c141306104bed8d
이진형 <122415954+LeeJ1nHyeong@users.noreply.github.com>
2024-09-06 11:13:52 +0900

    Title: 입력받아 계산 5, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240906/입력받아 계산 5/input-calculate-5.java
+++ 240906/입력받아 계산 5/input-calculate-5.java
@@ -2,14 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        StringTokenizer st = new StringTokenizer(s);
+        Scanner sc = new Scanner(System.in);
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
         System.out.println(a + b);
     }
