7ce05daf0d4993428d0b2ca065bb5b7487f402d2
이진형 <122415954+LeeJ1nHyeong@users.noreply.github.com>
2024-09-12 16:43:09 +0900

    Title: 10개 입력받기, Time: 106ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240912/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] numbers = br.readLine().split(" ");
+
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int number = Integer.parseInt(numbers[i]);
+
+            if (number == 0) {
+                break;
+            }
+
+            sum += number;
+            count++;
+        }
+
+        double average = (double) (sum / count);
+
+        System.out.println(sum + " " + String.format("%.1f", average));
+    }
+}
