a2fca44fa842d36b5549cbda902fdafde8469d7a
이진형 <122415954+LeeJ1nHyeong@users.noreply.github.com>
2024-09-12 16:38:11 +0900

    Title: 배열 채우기, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240912/배열 채우기/filling-array.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] numbers = br.readLine().split(" ");
+
+        int[] numArr = new int[10];
+        int count = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int number = Integer.parseInt(numbers[i]);
+            
+            if (number == 0) {
+                break;
+            }
+
+            numArr[i] = number;
+            count++;
+        }
+
+        for (int i = count - 1; i >= 0; i--) {
+            System.out.print(numArr[i] + " ");
+        }
+    }
+}
